17c8ce77af2cc161b2baa72400544075ef85b4a5
sanket <sanket.panda@viacom18.com>
2023-10-02 01:44:09 +0530

    Implement Searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,6 +23,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
