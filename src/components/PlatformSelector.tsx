1ef3f78f0b7fa814572c81ae8cd93537918040ac
sanket <sanket.panda@viacom18.com>
2023-10-03 17:14:42 +0530

    Exercise: Fetching Platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -17,7 +17,7 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- src/hooks/usePlatforms.tsx
+++ src/hooks/usePlatforms.tsx
@@ -1,4 +1,7 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import platfomrs from "../data/platfomrs";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +9,15 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platfomrs.length, results: platfomrs },
+  });
 
 export default usePlatforms;
