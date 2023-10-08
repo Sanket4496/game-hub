88146066649962abaf050f838051da8c91bf3763
sanket <sanket.panda@viacom18.com>
2023-10-08 10:38:12 +0530

    Refactor Entity Interfaces

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,8 @@
-import { Genre } from "./Genre";
-import { Platform } from "./Platform";
-import { Publisher } from "./Publisher";
+import Genre from "./Genre";
+import Platform from "./Platform";
+import Publisher from "./Publisher";
 
-export interface Game {
+export default interface Game {
   background_image: string;
   description_raw: string;
   genres: Genre[];
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 const usePlatforms = () =>
